bf49585f76fae3aabef4647165981fdb3f86151c
tof <asegliochristophe@gmail.com>
2021-06-27 17:05:10 +0200

    Exercices page 1 to 20

---
+++ Chapter_1.py
@@ -0,0 +1,77 @@
+Python 3.8.5 (default, May 27 2021, 13:30:53) 
+[GCC 9.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = y = 7
+>>> x
+7
+>>> y
+7
+>>> a, b = 4, 8.33
+>>> a
+4
+>>> b
+8.33
+>>> largeur = 20
+>>> hauteur = 5 * 9.3
+>>> largeur * hauteur
+930.0
+>>> #5.9*5*20 = 930.0#
+>>> # Display the text “Hello, World!” on the screen.
+print(“Hello, World!”)
+SyntaxError: invalid character in identifier
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> a = 3
+>>> b = 5
+>>> c = 7
+>>> a-b//c
+3
+>>> a+b//c
+3
+>>> a+b
+8
+>>> a-b
+-2
+>>> a-b/c
+2.2857142857142856
+>>> a=4
+>>> b=3
+>>> a//b
+1
+>>> a=9.5
+>>> b=5
+>>> a/b
+1.9
+>>> a//b
+1.0
+>>> # // est floordivision. it means that it round to the low unit
+>>> a, b = 7.3, 12
+>>> y = 3*a + b/5
+>>> y
+24.299999999999997
+>>> 10 % 3
+1
+>>> 10 % 5
+0
+>>> 45 % 6
+3
+>>> 20 % 4
+0
+>>> 21 % 6
+3
+>>> # 6 * 3 = 18   21 - 18 = 3
+>>> # Modulo %
+>>> r, pi = 12, 3.14159
+>>> s = pi * r**2
+>>> print(s)
+452.38896
+>>> print (type(r), type(pi), type(s))
+<class 'int'> <class 'float'> <class 'float'>
+>>> # fonction "type()" = entier, réel ...
+>>> h, m, s = 15, 27, 34
+>>> print("nombre de secondes écoulées depuis minuit = ", h*3600 + m*60 + s)
+nombre de secondes écoulées depuis minuit =  55654
+>>> # Coool !
+>>> 
